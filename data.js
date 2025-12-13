b566443b4f15b818b66412974dcb90be748c69b0
Weng Ti <wengti@hotmail.com>
2025-12-13 12:45:49 +0800

    First Commit - Basic HTML

---
+++ data.js
@@ -0,0 +1,25 @@
+const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        id: 0,
+        price: 14,
+        image: "./images/pizza.png"
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        image: "./images/burger.png",
+        id: 1
+    },
+        {
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        image: "./images/beer.png",
+        id: 2
+    }
+]
+
+export default menuArray
---
+++ index.js
@@ -0,0 +1,34 @@
+import menuArray from "./data.js";
+
+const itemOuter = document.getElementById("item-outer")
+
+render()
+
+
+function getInnerHtml(){
+    let innerHtml = []
+
+    innerHtml = menuArray.map( (menuItem) => {
+        const {name, ingredients, id, price, image} = menuItem
+
+        return `
+            <div class="item-inner">
+                <div class="food-img"><img src=${image}/></div>
+                <div class="food-detail">
+                    <p class="food-name">${name}</p>
+                    <p class="food-ingredient">${ingredients.join(", ")}</p>
+                    <p class="food-price">$${price}</p>
+                </div>
+                <div class="food-btn-container"><button>+</button></div>
+            </div>
+        `
+    }).join("")
+
+    return innerHtml
+}
+
+function render() {
+    itemOuter.innerHTML = getInnerHtml()
+}
+
+
