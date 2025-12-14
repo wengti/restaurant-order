3c8e10fbd5863a9bf70d37c91ccbf7b5f67599f1
Weng Ti <wengti@hotmail.com>
2025-12-14 11:22:54 +0800

    Create subpage for past order

---
+++ order.js
@@ -0,0 +1,34 @@
+// Local Storage
+let pastUserOrderArr = []
+if (localStorage.getItem("userOrderArr")){
+    pastUserOrderArr = JSON.parse(localStorage.getItem("userOrderArr"))
+} 
+
+// Get control of Elements
+const orderOuter = document.getElementById("order-outer")
+const footerContainer = document.getElementById("footer-container")
+const receiptIcon = document.getElementById("receipt-icon")
+
+// Render
+renderOrder()
+const dateNow = new Date()
+footerContainer.innerHTML = `<h2>Jimmy's Diner © ${dateNow.getFullYear()}</h2>`
+
+function renderOrder() {
+    orderOuter.innerHTML = getOrderPageInnerHtml()
+}
+
+function getOrderPageInnerHtml(){
+
+    // Dynamically add the past order count to the receipt icon
+    const orderNumberIcon = document.createElement("div")
+    orderNumberIcon.textContent = (pastUserOrderArr.length > 99) ? 99 : pastUserOrderArr.length
+    receiptIcon.appendChild(orderNumberIcon)
+
+
+    let orderPageInnerHtml = `
+        <p class="order-title">Your Order</p>
+    `
+
+    return orderPageInnerHtml
+}
